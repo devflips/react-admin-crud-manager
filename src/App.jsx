44ec211292ec5b09193815b22a28ed6864c934aa
Hafiz Muhammad Irfan <irfandlx19992@gmail.com>
2026-02-16 09:42:48 +0500

    npm pkj installation issue solve

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import CrudPage from "./components/CrudPage";
-
-export default function App() {
-  return <CrudPage />;
-}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CrudPage from "./components/CrudPage";
+import "./index.css";
+
+export default function Crud(props) {
+  return (
+    <>
+      <CrudPage config={props} />
+    </>
+  );
+}
