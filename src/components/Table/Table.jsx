264fd30a4d00a57de207bd360335a9b003bde8d6
Hafiz Muhammad Irfan <irfandlx19992@gmail.com>
2026-02-25 12:58:47 +0500

    alignment issue solve in table heading

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -379,7 +379,7 @@ const Table = ({ config, setShowAdd, title, buttonText, description }) => {
     <>
       {/* header Bar */}
 
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-3">
+      <div className="flex flex-col sm:flex-row sm:justify-between gap-4 mb-3">
         <div>
           <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
             {title}
